f5b13f5748c2090b0f1176b880195ec1a6f4a6a0
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-11-24 11:01:42 +0300

    create signin form

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:oxford_vocabulary_app/screens/auth.dart';
 
+void main() {
+  runApp(const VocabularyApp());
+}
+
+class VocabularyApp extends StatelessWidget {
+  const VocabularyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light(),
+      home: const AuthScreen(),
+    );
+  }
+}
---
+++ lib/widgets/signin_form.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class SignInForm extends StatefulWidget {
+  const SignInForm({super.key});
+
+  @override
+  State<SignInForm> createState() => _SignInFormState();
+}
+
+class _SignInFormState extends State<SignInForm> {
+  final _formKey = GlobalKey<FormState>();
+  var _passwordObscure = true;
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _passwordObscure = !_passwordObscure;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.email_outlined),
+                hintText: "Enter your email",
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: InputDecoration(
+                border: const OutlineInputBorder(),
+                prefixIcon: const Icon(Icons.lock_outline_rounded),
+                hintText: "Enter your password",
+                suffixIcon: IconButton(
+                    onPressed: _togglePasswordVisibility,
+                    icon: Icon(
+                      _passwordObscure
+                          ? Icons.visibility_off_outlined
+                          : Icons.visibility_outlined,
+                    )),
+              ),
+              obscureText: _passwordObscure,
+              keyboardType: TextInputType.text,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  alignment: Alignment.center,
+                  backgroundColor: Colors.blue,
+                  
+                ),
+                onPressed: () {},
+                child: const Text("Sign In"),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text("or"),
+          ],
+        ),
+      ),
+    );
+  }
+}
