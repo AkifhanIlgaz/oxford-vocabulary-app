6011da0101f11d8065d9c3daca3ff05f4bb88d5b
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-11-27 11:47:33 +0300

    make horizontal line dynamic with text

---
+++ lib/widgets/auth/horizontal_line_with_text.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:oxford_vocabulary_app/main.dart';
+
+class HorizontalOrLine extends StatelessWidget {
+  const HorizontalOrLine({
+    super.key,
+    this.height = 10,
+    required this.title,
+  });
+
+  final double? height;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Divider(
+              height: 10,
+              color: ProjectColors.mainColor,
+              endIndent: 10,
+              thickness: 1,
+            ),
+          ),
+          const Text(
+            "or",
+            style: TextStyle(color: Colors.blueGrey),
+          ),
+          Expanded(
+            child: Divider(
+              color: ProjectColors.mainColor,
+              indent: 10,
+              thickness: 1,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
